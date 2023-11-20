9b389a7f95b93a2262864d04b20296a3c9b8f923
Fiddelis <lucas.ruan@ges.inatel.br>
2023-11-20 18:42:21 -0300

    refatorando códigos

--- redeSocial/src/main/java/br/com/inatel/Main.java
+++ redeSocial/src/main/java/br/com/inatel/Main.java
@@ -1,14 +1,16 @@
 package br.com.inatel;
 
-import br.com.inatel.views.Menus;
+import br.com.inatel.views.TelaInicial;
 
 import java.util.Scanner;
 
 public class Main {
     public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        Menus menu = new Menus();
-        menu.inicial();
+        TelaInicial telaInicial = new TelaInicial();
+
+        telaInicial.exibir();
+
         sc.close();
     }
 }
---
+++ redeSocial/src/main/java/br/com/inatel/utils/Terminal.java
@@ -0,0 +1,5 @@
+package br.com.inatel.utils;
+
+public class Terminal {
+
+}
